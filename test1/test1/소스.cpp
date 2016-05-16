436d412524318a9e9774f3be6f5e8b388962235d
Cat613 <bob0613k@gmail.com>
2016-05-16 17:53:27 +0900

    새로운 함수 추가

--- test1/test1/소스.cpp
+++ test1/test1/소스.cpp
@@ -6,14 +6,15 @@
 using namespace std;
 
 
-typedef struct CharFrequency{
-	char character;
+typedef struct CharFrequency {
+	char* character;
 	int frequency;
 	struct CharFrequency * next;
 }CharFrequency;
 
 
 typedef struct HeapTree {
+	int index;
 	Tree* left;
 	Tree* right;
 	CharFrequency data;
@@ -22,66 +23,90 @@ typedef Tree * pTree;
 
 typedef CharFrequency * pCharFrequency;
 
-int heapArr[10000];
+Tree heapArr[10000] = { 0 };
+int tailIndex = 0;
+Tree* root = NULL;
+int* detectdirection(int index);
 void pushNode(char* word);
 Tree popNode();
 
 
-int main(){
+int main() {
 
-	string *str[100] = {  };
+	string *str[100] = {};
 	char cstr[1000];
-	char strall[10000]="";
-	char smallwords[10000]="";
+	char strall[10000] = "";
+	char smallwords[10000] = "";
 	char temp[10] = "";
 	char* result;
 
 	FILE *inputT = fopen("dataset.txt", "rt");
 
-	if (inputT == NULL){
+	if (inputT == NULL) {
 		puts("file open failed");
 		return -1;
 	}
 
-	
-	/*
-	while (fgets(cstr, sizeof(cstr), inputT) != NULL){
-		strlwr(cstr);
-		cout << cstr;
-	}
-	*/
-
-
 	// 파일에서 데이터를 입력받아서 strall에 넣는다.
-	while (fgets(cstr, sizeof(cstr), inputT) != NULL){
-		strcat(strall,cstr);
+	while (fgets(cstr, sizeof(cstr), inputT) != NULL) {
+		strcat(strall, cstr);
 	}
 	strlwr(strall);		//소문자로 치환
 
 
-	//알파벳 소문자와 띄어쓰기 간행문자를 제외한 문자를 smallwords에 넣는다.
-	for (int i = 0; i<strlen(strall); i++){
-		if ((strall[i]>='a'&&strall[i]<='z')||(strall[i]==' '||strall[i]=='\n')){
+						//알파벳 소문자와 띄어쓰기 간행문자를 제외한 문자를 smallwords에 넣는다.
+	for (int i = 0; i<strlen(strall); i++) {
+		if ((strall[i] >= 'a'&&strall[i] <= 'z') || (strall[i] == ' ' || strall[i] == '\n')) {
 			temp[0] = strall[i];
-			strcat(smallwords,temp);
+			strcat(smallwords, temp);
 		}
 	}
-	
-	
+
+
 	result = strtok(smallwords, " ");
-	while (result != NULL){
-		cout<<result<<endl;
+	while (result != NULL) {
+		cout << result << endl;
 		result = strtok(NULL, " ");
 	}
-	
 
-	if (feof(inputT) != 0){
+
+	if (feof(inputT) != 0) {
 		cout << "파일 출력 완료";
 	}
-	
-	
+
+
 	fclose(inputT);
 
+
+
+
+
+
+
 	return 0;
-	
+
+}
+
+
+
+int * detectdirection(int index) {
+	int temp[20], level[20];
+	int i;
+	for (i = 0; index>1; i++)
+	{
+		temp[i] = index % 2;
+	}
+	return nullptr;
+}
+
+void pushNode(char * word) {
+	if (root == NULL) {
+		root->data.character = word;
+		root->index = 1;
+		heapArr[1] = *root;
+		tailIndex++;
+	}
+	else {
+
+	}
 }
